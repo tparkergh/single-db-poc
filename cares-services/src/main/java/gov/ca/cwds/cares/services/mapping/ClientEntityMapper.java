ee354c4c0cc80e7f47a79e72d8f0cb2db73d7e46
shahid <shahid@192.168.0.7>
2018-12-27 10:15:47 -0800

    Fixed a mapstruct issue

--- cares-services/src/main/java/gov/ca/cwds/cares/services/mapping/ClientEntityMapper.java
+++ cares-services/src/main/java/gov/ca/cwds/cares/services/mapping/ClientEntityMapper.java
@@ -1,6 +1,7 @@
 package gov.ca.cwds.cares.services.mapping;
 
 import java.util.Collection;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import gov.ca.cwds.cares.persistence.entity.ClientEntity;
 import gov.ca.cwds.cares.services.interfaces.model.Client;
@@ -8,6 +9,7 @@ import gov.ca.cwds.cares.services.interfaces.model.Client;
 /**
  * CWDS J Team
  */
+@Mapper
 public interface ClientEntityMapper {
   
   public ClientEntityMapper INSTANCE = Mappers.getMapper(ClientEntityMapper.class);
