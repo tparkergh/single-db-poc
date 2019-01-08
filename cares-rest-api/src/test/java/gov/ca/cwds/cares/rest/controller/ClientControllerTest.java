3099bb9100925b60749d2f3ccd996b0f923f9aeb
shahid <shahid@192.168.0.7>
2019-01-08 00:54:37 -0800

    Cares service tests

--- cares-rest-api/src/test/java/gov/ca/cwds/cares/rest/controller/ClientControllerTest.java
+++ cares-rest-api/src/test/java/gov/ca/cwds/cares/rest/controller/ClientControllerTest.java
@@ -2,22 +2,21 @@ package gov.ca.cwds.cares.rest.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import gov.ca.cwds.cares.services.interfaces.api.ClientService;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(ClientController.class)
+//@AutoConfigureWebMvc
+//@AutoConfigureMockMvc
+//@WebMvcTest(ClientController.class)
+//@ContextConfiguration
+//@SpringBootTest
 public class ClientControllerTest {
   
-  @Autowired
-  private MockMvc mvc;
-
-  @MockBean
-  private ClientService clientService;
+//  @Autowired
+//  private MockMvc mvc;
+//
+//  @MockBean(name = "ClientService")
+//  private ClientService clientService;
   
   @Test
   public void test() throws Exception {
