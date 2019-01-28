4a51bd8485dcb1f87f451e412cbc63cb44974f27
Tyler Clemens <Tyler.Clemens@osi.ca.gov>
2019-01-28 10:42:34 -0800

    LS-159 update reducer to be a single reducer
    it handles:
    - referrals
    - clients
    - allegations
    - system codes

--- cares-ui/src/actions/index.js
+++ cares-ui/src/actions/index.js
@@ -1,5 +1,24 @@
-export const GET_REFERRALS = 'GET_REFERRALS'
-export const getReferrals = payload => ({
-  type: GET_REFERRALS,
+export const SET_REFERRALS = 'SET_REFERRALS'
+export const SET_CLIENT = 'SET_CLIENTS'
+export const OPEN_REFERRAL = 'OPEN_REFERRAL'
+export const SET_SYSTEM_CODES = 'SET_SYSTEM_CODES'
+
+export const setReferrals = payload => ({
+  type: SET_REFERRALS,
   payload
+})
+
+export const setClient = payload => ({
+  type: SET_CLIENT,
+  payload
+})
+
+export const setSystemCodes = payload => ({
+  type: SET_SYSTEM_CODES,
+  payload
+})
+
+export const openReferral = (id) => ({
+  type: OPEN_REFERRAL,
+  id
 })
--- cares-ui/src/reducers/referrals.js
+++
@@ -1,9 +0,0 @@
-import { GET_REFERRALS } from '../actions'
-export const referrals = (state = [], action) => {
-  switch (action.type) {
-    case GET_REFERRALS:
-      return state
-    default:
-      return state
-  }
-}
