ac51411068589cee3d5f969b7b17eab297bfc89a
Tyler Clemens <Tyler.Clemens@osi.ca.gov>
2019-01-29 10:53:13 -0800

    LS-177 correct route to get all system metas

--- cares-ui/src/actions/index.js
+++ cares-ui/src/actions/index.js
@@ -1,6 +1,7 @@
 export const SET_REFERRALS = 'SET_REFERRALS'
 export const SET_CLIENT = 'SET_CLIENTS'
 export const OPEN_REFERRAL = 'OPEN_REFERRAL'
+export const SET_SYSTEM_METAS = 'SET_SYSTEM_METAS'
 export const SET_SYSTEM_CODES = 'SET_SYSTEM_CODES'
 
 export const setReferrals = payload => ({
@@ -10,6 +11,11 @@ export const setReferrals = payload => ({
 
 export const setClient = payload => ({
   type: SET_CLIENT,
+  payload
+})
+
+export const setSystemMetas = payload => ({
+  type: SET_SYSTEM_METAS,
   payload
 })
 
