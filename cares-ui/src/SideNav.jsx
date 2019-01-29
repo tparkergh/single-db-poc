16e2ba79cfe0b60fb292e3b92213ca81e8e9f5a3
Tyler Clemens <Tyler.Clemens@osi.ca.gov>
2019-01-29 14:26:38 -0800

    LS-177 refactor sidenav to have no component state

--- cares-ui/src/SideNav.jsx
+++ cares-ui/src/SideNav.jsx
@@ -1,7 +1,10 @@
 import { Nav, NavItem, NavLink } from '@cwds/reactstrap'
 import React from 'react'
 import { connect } from 'react-redux'
-import { selectReferrals } from './selectors'
+import {
+  selectReferrals,
+  selectCurrentReferralId
+} from './selectors'
 import axios from 'axios'
 import { getAllReferralsRoute } from './routes'
 import {
@@ -19,8 +22,15 @@ class SideNav extends React.Component {
     axios({
       url: getAllReferralsRoute(),
       method: 'get'
-    }).then((response) => this.props.setReferrals(response.data))
+    }).then((response) => {
+      this.props.setReferrals(response.data)
+      if (this.props.currentReferralId === undefined) {
+        const referralId = this.props.referrals[0].id
+        this.props.openReferral(referralId)
+      }
+    })
   }
+
   render () {
     const referrals = this.props.referrals
     return (
@@ -31,16 +41,15 @@ class SideNav extends React.Component {
             key={`nav-item-referral-${id}`}
             onClick={ ({target}) => {
               this.props.openReferral(id)
-              this.setState({ activeIndex: index})
             }}
           >
-          { this.isActive(index) &&
+          { this.isActive(id) &&
             <NavLink className={`referral-${id}`} active>
               <div>{ name }</div>
               <div className='id'>ID: { id }</div>
             </NavLink>
           }
-          { !this.isActive(index) &&
+          { !this.isActive(id) &&
             <NavLink className={`referral-${id}`}>
               <div>{ name }</div>
               <div className='id'>ID: { id }</div>
@@ -53,8 +62,8 @@ class SideNav extends React.Component {
     )
   }
 
-  isActive(index) {
-    return this.state.activeIndex === index
+  isActive(id) {
+    return this.props.currentReferralId === id
   }
 }
 
@@ -62,7 +71,8 @@ const mapStateToProps = (state, ownProps) => ({
   referrals: selectReferrals(state).map((referral) => ({
     id: referral.identifier,
     name: referral.referral_name
-  }))
+  })),
+  currentReferralId: selectCurrentReferralId(state)
 })
 
 const mapDispatchToProps = { setReferrals, openReferral }
