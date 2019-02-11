184fa1783d7fb1880fe29f853ea828b6b492ce24
Tyler Clemens <Tyler.Clemens@osi.ca.gov>
2019-02-11 15:04:49 -0800

    REP-16 change the App to show the landing page first

--- cares-ui/test/App.test.js
+++ cares-ui/test/App.test.js
@@ -1,21 +1,20 @@
 import { shallow } from 'enzyme'
-import App from '../src/App'
-import Main from '../src/Main'
+import { App } from '../src/App'
+import LandingPage from '../src/LandingPage'
 import SideNav from '../src/SideNav'
 import JsonBRE from 'JsonBRE'
 
-xdescribe('App', () => {
-  it('should render a page layout with side navigation and main area', () => {
-    const app = shallow(<App/>)
+describe('App', () => {
+  it('should render a page layout with a Landing Page', () => {
+    const app = shallow(<App />, { disableLifecycleMethods: true })
     const page = app.find('Page')
     expect(page).toBeDefined()
-    expect(page.props().main).toBe(Main)
-    expect(page.props().sidenav).toBe(SideNav)
+    expect(page.props().main).toBe(LandingPage)
   })
 
-  it('should define all the rules when the component is mounted', () => {
+  xit('should define all the rules when the component is mounted', () => {
     const engine = new JsonBRE()
-    const app = shallow(<App/>)
+    const app = shallow(<App />, { disableLifecycleMethods: true })
     expect(Object.keys(engine.rules)).toContain('first-name-required-rule')
     expect(Object.keys(engine.rules)).toContain('last-name-required-rule')
   })
