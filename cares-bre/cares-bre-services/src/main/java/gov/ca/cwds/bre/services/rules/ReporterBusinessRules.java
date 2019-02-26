35c538475913daca88d6e65c9b9371996b3f5665
jim lebeau <James.LeBeau@osi.ca.gov>
2019-02-26 13:20:07 -0800

    REP-48 Reporter business rule documentation

---
+++ cares-bre/cares-bre-services/src/main/java/gov/ca/cwds/bre/services/rules/ReporterBusinessRules.java
@@ -0,0 +1,16 @@
+package gov.ca.cwds.bre.services.rules;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author CWDS J Team
+ */
+@Component("ReporterBusinessRules")
+public class ReporterBusinessRules extends DroolsBusinessRuleBase {
+
+  public ReporterBusinessRules() {
+    super();
+
+  }
+
+}
