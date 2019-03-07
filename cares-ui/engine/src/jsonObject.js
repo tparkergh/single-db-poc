daec27acd5a484090d5b67cb4474f8414a99dbe2
Tyler Clemens <Tyler.Clemens@osi.ca.gov>
2019-03-07 08:39:47 -0800

    Fix missing and missing_some implementation in the engine

--- cares-ui/engine/src/jsonObject.js
+++ cares-ui/engine/src/jsonObject.js
@@ -1,6 +1,8 @@
 import JsonMap from './jsonMap'
 import JsonEntity from './jsonEntity'
 import JsonSelector from './jsonSelector'
+import JsonSelectorArray from './jsonSelectorArray'
+import JsonSome from './jsonSome'
 import {
   ARRAY_OPERATIONS,
   DATA_OPERATIONS
@@ -13,6 +15,12 @@ export default class JsonObject {
 
     if (ARRAY_OPERATIONS.includes(operator)) {
       return new JsonMap(this.value)
+    }
+    if (operator === 'missing') {
+      return new JsonSelectorArray(this.value)
+    }
+    if (operator === 'missing_some') {
+      return new JsonSome(this.value)
     }
     if (DATA_OPERATIONS.includes(operator)) {
       return new JsonSelector(this.value)
